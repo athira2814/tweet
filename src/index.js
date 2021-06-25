1dc95beb056dafab6a9540fd06a665c3003a8688
athira2814 <athirad193@gmail.com>
2021-06-25 13:54:11 +0530

    setup router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from  "./pages/Home"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import CreateTweet from "./pages/CreateTweet";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route component={Home} path="/" exact></Route>
+      <Route component={CreateTweet} path="/create" exact></Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
-
