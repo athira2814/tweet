55b6c5fa7d3aa72bead0c073ef524acf718b893d
athira2814 <athirad193@gmail.com>
2021-06-26 19:21:39 +0530

    Updated createtweet

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React , { useState } from "react";
 
 function Card() {
+  const [name , setName ] = useState("");
+  const [tweet , setTweet ] = useState("");
+  function createtweet(){
+    console.log(name,tweet)
+  }
   return (
-    <>
+
       <div className="shadow">
-        <input name="name" placeholder="Name" />
+        <input name="name" placeholder="Name" onChange={e => setName(e.target.value)}/>
           <textarea
-            maxLength="200"
+            maxLength="300"
             className="form-floating"
             placeholder="What's in your mind...."
             rows="3"
+            onChange= {e => setTweet(e.target.value)}
           ></textarea>
-        <button type="button" className="tweet-btn">
+        <button type="button" className="tweet-btn" onClick = {createtweet()} >
           Tweet
         </button>
       </div>
-    </>
   );
 }
 
